a406ab6ff97ee0d7ea26f39014e0dbc75b2c0296
Kevin Yang <kevin.zj.yang@outlook.com>
2020-10-23 16:54:47 +0800

    Update One-click-Packaging.cmd

--- One-click-Packaging.cmd
+++ One-click-Packaging.cmd
@@ -1,2 +1,4 @@
-pip install -r requirements.txt
-pyinstaller -F -i docs\logo.ico foo\_main_.py
+::python安装路径自己修改
+C:\Users\yzj03\AppData\Local\Programs\Python\Python39\python.exe -m pip install -r requirements.txt
+C:\Users\yzj03\AppData\Local\Programs\Python\Python39\python.exe -m PyInstaller -F -i docs\logo.ico foo\_main_.py
+pause
